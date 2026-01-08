6f988ead5c8b51147a7edf8c5869082ab7dd3549
Saphal-1 <gamersaphal8@gmail.com>
2026-01-08 19:59:08 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("supportBtn").addEventListener("click", () => {
+  alert("Support Email:\n\n📧 gamersaphal8@gmail.com");
+});
+
+document.getElementById("privacyBtn").addEventListener("click", () => {
+  alert(
+    "Privacy Policy\n\nThis website does not collect personal data.\nYour privacy is fully respected."
+  );
+});
